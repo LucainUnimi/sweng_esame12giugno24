aabe355e0b3f08475bfc589283e15d7ec0ad2f5c
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-12-17 15:54:48 +0100

    VERDE: add Book to model

--- src/main/java/it/unimi/di/sweng/esame/model/Model.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Model.java
@@ -1,11 +1,18 @@
 package it.unimi.di.sweng.esame.model;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Model implements State{
+
+    @NotNull private final List<Booking> bookings = new ArrayList<>();
+
     @Override
     public List<Booking> getByTime() {
-        return List.of();
+        return bookings;
     }
 
     @Override
@@ -19,6 +26,6 @@ public class Model implements State{
     }
 
     public void addBook(Time t, ClassRoom c) {
-
+        bookings.add(new Booking(t, c));
     }
 }
